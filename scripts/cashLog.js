b3a2fa12cc7dfc1ee40270012a87bdcf85c77f3f
Zack Sheppard <z@zack.io>
2015-06-14 10:58:15 -0400

    cashLog saves transactions

---
+++ scripts/cashLog.js
@@ -0,0 +1,42 @@
+// Description:
+//   Logs my cash spent. Mint captures a lot but not my cash.
+//   
+//
+
+//   
+// Dependencies:
+//   
+
+module.exports = function(robot){
+
+  function getTransactions(user){
+    // Create a seperate function
+  }
+
+  function saveTransaction(user, amount, category, time){
+    var transactions = user["cash-transactions"] || [];
+    var newTransaction = {
+      amount: amount,
+      category: category
+    };
+
+    transactions.push(newTransaction);
+    robot.brain.save();
+  }
+    
+  robot.respond(/\$(\d+) (.*)/, function(msg){
+    console.log(
+      "msg.match[0]: " + msg.match[0] + "\n" + 
+      "msg.match[1]: " + msg.match[1] + "\n" + 
+      "msg.match[2]: " + msg.match[2] + "\n"
+    );
+    var amount = msg.match[1];
+    var category = msg.match[2];
+
+    var id = msg.message.user.id
+    var user = robot.brain.userForId(id);
+
+    saveTransaction(user, amount, category);
+    msg.send("Got it. You spent $"+ user["cash-transactions"].slice(-1)[0]['amount'] + " on " + user["cash-transactions"].slice(-1)[0]['category'] + "." );
+  });
+}
