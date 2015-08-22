4c52a301ee8d187b18383578c670df0df82b9a42
Zack Sheppard <z@zack.io>
2015-08-22 13:41:24 -0400

    comment out userapi.js

--- scripts/userApi.js
+++ scripts/userApi.js
@@ -12,27 +12,27 @@ var app = express();
 
 module.exports = function(robot) {
   
-  robot.router.get('/api/users/:id', function(req, res) {
-    var id = req.params.id;
-    var user = robot.brain.userForId(id);
-    userObj = JSON.stringify(user);
-    return res.send(userObj);
-  });
+  // robot.router.get('/api/users/:id', function(req, res) {
+  //   var id = req.params.id;
+  //   var user = robot.brain.userForId(id);
+  //   userObj = JSON.stringify(user);
+  //   return res.send(userObj);
+  // });
 
-  robot.router.put('/api/users/:id', function(req, res) {
-    var id = req.params.id;
-    var user = robot.brain.userForId(id);
-    console.log(req.body);
-    return res.send("I'm not updating anyone yet. But fix this method and I can update " + user.name +  "for you next time.");
-  });
+  // robot.router.put('/api/users/:id', function(req, res) {
+  //   var id = req.params.id;
+  //   var user = robot.brain.userForId(id);
+  //   console.log(req.body);
+  //   return res.send("I'm not updating anyone yet. But fix this method and I can update " + user.name +  "for you next time.");
+  // });
 
-  // Uses the `userForEmail` function added to hubot/src/brain.coffee
-  robot.router.get('/api/users/findBy/email/:email', function(req, res) {
-    var email = req.params.email;
-    var user = robot.brain.userForEmail(email);
-    var userObj = JSON.stringify(user);
-    return res.send('{"id":"'+ user['id'] +'"}');
-  });
+  // // Uses the `userForEmail` function added to hubot/src/brain.coffee
+  // robot.router.get('/api/users/findBy/email/:email', function(req, res) {
+  //   var email = req.params.email;
+  //   var user = robot.brain.userForEmail(email);
+  //   var userObj = JSON.stringify(user);
+  //   return res.send('{"id":"'+ user['id'] +'"}');
+  // });
 
 
 };
