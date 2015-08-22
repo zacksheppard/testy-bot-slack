960aa1003dc1ebb0cf367aabec068d443cecff74
Zack Sheppard <z@zack.io>
2015-08-22 14:16:45 -0400

    userApi: comment out user put route

--- scripts/userApi.js
+++ scripts/userApi.js
@@ -16,12 +16,12 @@ module.exports = function(robot) {
     return res.send(userObj);
   });
 
-  robot.router.put('/api/users/:id', function(req, res) {
-    var id = req.params.id;
-    var user = robot.brain.userForId(id);
-    console.log(req.body);
-    return res.send("I'm not updating anyone yet. But fix this method and I can update " + user.name +  "for you next time.");
-  });
+  // robot.router.put('/api/users/:id', function(req, res) {
+  //   var id = req.params.id;
+  //   var user = robot.brain.userForId(id);
+  //   console.log(req.body);
+  //   return res.send("I'm not updating anyone yet. But fix this method and I can update " + user.name +  "for you next time.");
+  // });
 
   // Uses the `userForEmail` function added to hubot/src/brain.coffee
   robot.router.get('/api/users/findBy/email/:email', function(req, res) {
