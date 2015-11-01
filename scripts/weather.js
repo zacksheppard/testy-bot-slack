29066308239285b54479cc23ee4a5400de682c76
Zack Sheppard <z@zack.io>
2015-11-01 17:47:19 -0500

    Weather: Conditional added to handle multiple results

--- scripts/weather.js
+++ scripts/weather.js
@@ -27,16 +27,29 @@ module.exports = function(robot){
       '/geolookup/conditions/forecast/q/' + 
       format_location(location) + '.json';
 
-    return msg.http(url).get()(function(err, res, body){
+    return robot.http(url).get()(function(err, res, body) {
       var data = JSON.parse(body);
-      var city = data.current_observation.display_location.full;
-      var temp = data.current_observation.temperature_string;
-      var feels_like = data.current_observation.feelslike_string;
-      var humidity = data.current_observation.relative_humidity;
-      var forecast_title = data.forecast.txt_forecast.forecastday[0].title;
-      var forecast = data.forecast.txt_forecast.forecastday[0].fcttext;
+      if (data.location != null) {
+        console.log(data.location);
+        var city = data.current_observation.display_location.full;
+        var temp = data.current_observation.temperature_string;
+        var feels_like = data.current_observation.feelslike_string;
+        var humidity = data.current_observation.relative_humidity;
+        var forecast_title = data.forecast.txt_forecast.forecastday[0].title;
+        var forecast = data.forecast.txt_forecast.forecastday[0].fcttext;
 
-      return msg.send('Currently it\'s ' + temp + ' in ' + city + '. ' + 'For ' + forecast_title + ', ' + forecast);
+        return msg.send('Currently it\'s ' + temp + ' in ' + city + '. ' + 'For ' + forecast_title + ', ' + forecast);
+      } else if (data.response.results != null){
+        var results = data.response.results;
+        var response = 'More than one result for ' + location + '. Prob better to *use zip code* until Zack programs me to choose one. Here\'s what I found, fwiw: \n';
+        for (var i = 0; i < results.length; i++) {
+          var result = '* ' + results[i].city + ', ' + results[i].state + ', ' + results[i].country + '\n'; 
+          response += result;
+        }
+        return msg.send(response);
+      }
+
     });
+
   }); 
 }
