2eb7394fc436e1aa8bf6ceccab2a6ae5c14a8020
Zack Sheppard <z@zack.io>
2015-11-01 14:29:28 -0500

    Weather: Give a more complete weather description for the day

--- scripts/weather.js
+++ scripts/weather.js
@@ -17,14 +17,26 @@ module.exports = function(robot){
   robot.respond(/weather (.*)/, function(msg){
     var location = msg.match[1];
     
-    var conditions_url = 
-      'http://api.wunderground.com/api/725b139083f86186/conditions/q/' + location + '.json';
+    var format_location = function(location){
+      return location.split(' ').join('_');
+    };
 
-    return msg.http(conditions_url).get()(function(err, res, body){
+    var url = 
+      'http://api.wunderground.com/api/' +
+      '725b139083f86186' + 
+      '/geolookup/conditions/forecast/q/' + 
+      format_location(location) + '.json';
+
+    return msg.http(url).get()(function(err, res, body){
       var data = JSON.parse(body);
-      var temp = data.current_observation.temperature_string;
       var city = data.current_observation.display_location.full;
-      return msg.send('The current temp in ' + city + ' is ' + temp + '.');
+      var temp = data.current_observation.temperature_string;
+      var feels_like = data.current_observation.feelslike_string;
+      var humidity = data.current_observation.relative_humidity;
+      var forecast_title = data.forecast.txt_forecast.forecastday[0].title;
+      var forecast = data.forecast.txt_forecast.forecastday[0].fcttext;
+
+      return msg.send('Currently it\'s ' + temp + ' in ' + city + '. ' + 'For ' + forecast_title + ', ' + forecast);
     });
   }); 
 }
