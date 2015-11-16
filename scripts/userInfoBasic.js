06018903dd79f2fc55a5aa8719a55017e4d0ea96
Zack Sheppard <z@zack.io>
2015-11-16 08:21:26 -0500

    weather watch regex done

--- scripts/userInfoBasic.js
+++ scripts/userInfoBasic.js
@@ -19,16 +19,48 @@
 module.exports = function(robot){
 
   var scaffoldProfile = function(userId){
-    var user = robot.brain.userForId(userId);
+  var user = robot.brain.userForId(userId);
     user.profile = {
       locations: {
         home: {
-          zipcode: '',
-          privacy: 'private'
-        }
+          city: '',
+          state: '',
+          country_code: '',
+          postal_code: '',
+          tz: '',
+          tz_label: '',
+          tz_offset: ''
+        } 
       }
     };
   }; 
+
+  var setTimeZone = function(userId){
+    var user = robot.brain.userForId(userId);
+    var url = "https://slack.com/api/users.list?token=" + 
+    process.env.HUBOT_SLACK_TOKEN;
+
+    if(!user.profile){
+      scaffoldProfile(userId);
+    }
+
+    robot.http(url).get()(function(err, res, body) {
+      var data = JSON.parse(body);
+      for(var i=0; i < data.members.length; i++){
+        if (data.members[i].id === userId){
+          user.profile.locations.home.tz = data.members[i].tz;
+          user.profile.locations.home.tz_label = data.members[i].tz_label;
+          user.profile.locations.home.tz_offset = data.members[i].tz_offset;
+        }
+      }
+      console.log("Timezone set to " +
+        "tz: " + user.profile.locations.home.tz + "\n" +
+        "tz_label: " + user.profile.locations.home.tz_label + "\n" +
+        "tz_offset: " + user.profile.locations.home.tz_offset + "\n"
+      );
+    });
+
+  };
 
   robot.respond(/profile location add (.*)/i, function(msg){
     // console.log(msg);
@@ -48,16 +80,15 @@ module.exports = function(robot){
       " as  " + user.profile.locations[locationName] + ".");
   });
 
-
-  // robot.respond(/my car is a (\w+)/i, function(msg){
-  //   console.log("Message from user with id: " + msg.message.user.id);
-  //   var car = msg.match[1];
-  //   id = msg.message.user.id
-  //   var user = robot.brain.userForId(id);
-  //   user['car'] = car;
-  //   robot.brain.set('car', car);
-  //   robot.brain.save();
-  //   msg.send("Got it. You drive a " + car + ".");
-  // });
+  robot.respond(/set timezone/i, function(msg){
+    var userId = msg.message.user.id;
+    var user = robot.brain.userForId(userId);
+    setTimeZone(userId);
+    msg.send(
+      "tz: " + user.profile.locations.home.tz + "\n" +
+      "tz_label: " + user.profile.locations.home.tz_label + "\n" +
+      "tz_offset: " + user.profile.locations.home.tz_offset + "\n"
+    );
+  });
 
 }
